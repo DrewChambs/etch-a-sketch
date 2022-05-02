3d99206ac7ee1e93ff061e98c2c37120ac8c3808
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-05-01 22:31:38 -0400

    Interface created. Sketch grid added. Initial JS for grid creation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log("working!");
+// Link to container
+const mainContainer = document.querySelector(".wrapper");
+
+// Create div elements
+let div;
+let total = 98 * 98;
+for (let i = 0; i < total; i++) {
+  div = document.createElement("div");
+  div.setAttribute("class", "etch-box");
+  // div.setAttribute("class", "change_grid");
+  mainContainer.appendChild(div);
+}
+// Link to grid
+const etchBox = document.querySelectorAll(".etch-box");
+
+// Add Event listener to grid
+etchBox.forEach(item => {
+  item.addEventListener("mouseover", () => {
+    console.log("workin!");
+    item.style.backgroundColor = "black";
+  });
+});
+
+console.log("sillll");
