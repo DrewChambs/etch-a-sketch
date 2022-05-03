311da6e3b1d86ef8d65da4aad278cab931f5f9e5
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-05-03 13:49:54 -0400

    Reset button enabled. Document reload added to set default again. console.log's removed

--- js/app.js
+++ js/app.js
@@ -11,30 +11,25 @@ userSelectBtn.addEventListener("click", () => {
   let selection = prompt("enter num");
 
   if (selection > 100) {
-    return alert("Number can not be mire than 100");
+    return alert("Enter a number no more than 100");
   } else {
     printNewGrid(selection);
   }
-
-  // userSelectBtn.disabled = true;
-  // userSelectBtn.classList.add("disableButton");
 });
 
 resetBtn.addEventListener("click", () => {
-  console.log("reset");
-  // userSelectBtn.enabled = true;
-  // userSelectBtn.classList.remove("disableButton");
+  wrapperContainer.innerHTML = "";
+  location.reload();
 });
 
 // Default Grid
 function startingGrid() {
-  console.log("starting grid");
   for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.setAttribute("class", "etch-box");
     wrapperContainer.appendChild(div);
   }
-  // Link to newly created
+  // Link to newly created grid
   const etchBox = document.querySelectorAll(".etch-box");
   // Add EventListener
   etchBox.forEach(item => {
@@ -46,9 +41,11 @@ function startingGrid() {
 
 // User-defined Grid
 function printNewGrid(selection) {
+  // Clear container
   wrapperContainer.innerHTML = "";
+
+  // Load user grid
   let total = selection * selection;
-  // wrapperContainer.style.display = `grid`;
   wrapperContainer.style.gridTemplateColumns = `repeat(${selection}, 1fr)`;
   wrapperContainer.style.gridTemplateRows = `repeat(${selection}, 1fr)`;
   for (let i = 0; i < total; i++) {
@@ -56,6 +53,7 @@ function printNewGrid(selection) {
     div.setAttribute("class", "etch-box");
     wrapperContainer.appendChild(div);
   }
+
   // Link to newly created
   const etchBox = document.querySelectorAll(".etch-box");
   // Add EventListener
