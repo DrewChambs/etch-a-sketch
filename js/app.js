2cdc68f7de8fc3d2af6bea2c764849a2c7d1b780
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-05-03 13:26:57 -0400

    Refactored JS to clean code. CSS styles for new JS

--- js/app.js
+++ js/app.js
@@ -1,45 +1,67 @@
-// Link to body section
-const bodyEtch = document.querySelector(".body");
-// Link to sketch container
+// Link to wrapper for game
 const wrapperContainer = document.querySelector(".wrapper");
+const userSelectBtn = document.querySelector(".userSelectBtn");
+const resetBtn = document.querySelector(".resetBtn");
 
-// Default Setup
-let defaultSetup = 16;
-let defaultTotal = defaultSetup * defaultSetup;
+// Launch Default
+startingGrid();
 
-// Get User selection
-let selection = 16;
-let userTotal = selection * selection;
+// User selection
+userSelectBtn.addEventListener("click", () => {
+  let selection = prompt("enter num");
 
-// div element
-let div;
+  if (selection > 100) {
+    return alert("Number can not be mire than 100");
+  } else {
+    printNewGrid(selection);
+  }
 
-//// Build Grid ////
-for (let i = 0; i < userTotal; i++) {
-  // Create divs for grid
-  div = document.createElement("div");
-  div.setAttribute("class", "etch-box");
-  wrapperContainer.appendChild(div);
-}
-// paintGrid(selection);
-// Link to grid
-const etchBox = document.querySelectorAll(".etch-box");
-
-// Event listener to paint grid
-etchBox.forEach(item => {
-  item.addEventListener("mouseover", () => {
-    item.style.backgroundColor = "black";
-  });
+  // userSelectBtn.disabled = true;
+  // userSelectBtn.classList.add("disableButton");
 });
 
-paintGrid(selection);
+resetBtn.addEventListener("click", () => {
+  console.log("reset");
+  // userSelectBtn.enabled = true;
+  // userSelectBtn.classList.remove("disableButton");
+});
 
-// Paint grid function
-function paintGrid(selection) {
-  wrapperContainer.style.display = `grid`;
-  wrapperContainer.style.gridTemplateColumns = `repeat(${selection}, 1fr)`;
-  wrapperContainer.style.gridTemplateRows = `repeat(${selection}, 1fr)`;
+// Default Grid
+function startingGrid() {
+  console.log("starting grid");
+  for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "etch-box");
+    wrapperContainer.appendChild(div);
+  }
+  // Link to newly created
+  const etchBox = document.querySelectorAll(".etch-box");
+  // Add EventListener
+  etchBox.forEach(item => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "black";
+    });
+  });
 }
 
-// wrapperContainer.style.gridTemplateColumns = `repeat(${userTotal}, 1fr)`;
-// wrapperContainer.style.gridTemplateRows = `repeat(${userTotal}, 1fr)`;
+// User-defined Grid
+function printNewGrid(selection) {
+  wrapperContainer.innerHTML = "";
+  let total = selection * selection;
+  // wrapperContainer.style.display = `grid`;
+  wrapperContainer.style.gridTemplateColumns = `repeat(${selection}, 1fr)`;
+  wrapperContainer.style.gridTemplateRows = `repeat(${selection}, 1fr)`;
+  for (let i = 0; i < total; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "etch-box");
+    wrapperContainer.appendChild(div);
+  }
+  // Link to newly created
+  const etchBox = document.querySelectorAll(".etch-box");
+  // Add EventListener
+  etchBox.forEach(item => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "black";
+    });
+  });
+}
